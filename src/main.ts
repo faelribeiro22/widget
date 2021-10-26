85c6b3d3f58da2d7c2576733e32e5a85e8ed4a42
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2021-10-26 01:59:04 -0300

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import '@/assets/css/tailwind.css'
+import '@/assets/css/font.css'
+import 'animate.css'
 
 createApp(App).mount('#app')
